764854931cf688e3a91cb6891c63937465dc43bd
gayoenLee <leeedho67@gmail.com>
2020-04-30 01:55:16 +0900

    [ADD]메인 페이지에서 로그인 후 세션유지하는 코드 작성

--- mainPage.php
+++ mainPage.php
@@ -1,3 +1,7 @@
+<?php
+include 'config.php'
+?>
+
 <!DOCTYPE html>
 
 <head>
@@ -10,7 +14,6 @@
 
 <body>
 
-
     <div class="wrapper">
         <h1>LOGO</h1>
         <nav>
@@ -20,8 +23,36 @@
                 <li><a href="imageBoardPage.php">BOARD</a></li>
                 <li><a href="newsPage.html">NEWS</a></li>
                 <li><a href="goodsPage.html">GOODS</a></li>
+                <?php
+                if(!$userid){
+                    ?>
+                    <ul>
+                    <li>
+                    <a href='#' role="button">접속하기<span class="caret"></span></a>
+                    <ul>
+                    <li><a href="login.php">로그인</a></li>
+                    </li>
+                    <li><a href="joinMember.php">회원가입</a></li>
+                    </ul>
+                    </li>
+                    </ul>
+                    <?php
+                }else{
+$logged = $username."(".$userid.")";
+                    ?>
+                    <ul>
+                    <li>
+                    <a href="#" role="button"><b><?=$logged?></b>님의 회원관리
+                    <span class="caret"></span></a>
+<ul>
+<li><a href="logout.php">로그아웃</a></li>
+</ul>
 
-            </ul>
+                    </li>
+                    </ul>
+                    <?php
+                }?>
+                   </div> 
         </nav>
     </div>
     <span class="username">WELCOME !</span>
