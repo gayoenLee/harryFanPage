0a3532310831d90a904a5e16f6cffb0819461d87
gayoenLee <leeedho67@gmail.com>
2020-04-30 17:02:27 +0900

    [MOD]글쓰기 데이터 디비로 저장하도록 바꿈

--- writeImageProcess.php
+++ writeImageProcess.php
@@ -1,15 +1,38 @@
 <?php
-ob_start();
-$userName=$_POST['userName'];
-$contents=$_POST['contents'];
-$title=$_POST['title'];
-$password=$_POST['password'];
-$boardTime=$_POST['boardTime'];
+// ob_start();
+// $userName=$_POST['userName'];
+// $contents=$_POST['contents'];
+// $title=$_POST['title'];
+// $password=$_POST['password'];
+// $boardTime=$_POST['boardTime'];
 
-$boardArray = array($title, $userName, $contents, $password, $boardTime);
-$data = serialize($boardArray);
+// $boardArray = array($title, $userName, $contents, $password, $boardTime);
+// $data = serialize($boardArray);
 
-file_put_contents('imageBoardData/'.$_POST["title"], $data);
-header('Location: /imageBoardPage.php?title='.$_POST["title"]);
+// file_put_contents('imageBoardData/'.$_POST["title"], $data);
+// header('Location: /imageBoardPage.php?title='.$_POST["title"]);
+session_start();
+include "config.php";
+include "dbConnect.php";
+//세션에 저장된 아이디값을 name에 저장.
+$name = $userid;
+$date = date('Y-m-d');
+$userPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
+//앞에서 포스트로 받은 값들 저장.
+$title = $_POST['title'];
+$content = $_POST['content'];
+
+database(
+    "INSERT talkBoard SET 
+    name = '".$name."';
+    password = '".$userPassword."';
+    title = '".$title."';
+    content = '".$content."';
+    time = '".$date."';
+");
 
 ?>
+<script>
+    alert("글쓰기가 완료되었습니다.");
+    location.href = 'imageBoardPage.php';
+</script>
