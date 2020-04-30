1b81a41cde494af69e59e668f734d9c1bbfdb50f
gayoenLee <leeedho67@gmail.com>
2020-04-30 16:39:56 +0900

    [MOD]로그인한 경우에만 글쓰기 가능하도록 세션 유지, ui변하도록 작성.

--- writeImageBoard.php
+++ writeImageBoard.php
@@ -1,18 +1,52 @@
 <?php
+session_start();
 include "config.php";
 include "dbConnect.php";
-include "loginOK.php";
+//아래 코드 지웠더니 로그인 상태 확인됨.
+// include "loginOK.php";
+$id = $_POST['id'];
 ?>
 <!DOCTYPE html>
 <head>
     <title>이미지 공유 게시판</title>
     <link rel="stylesheet" type="text/css" href="showImageBoardCSS.css"/>
-    <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script> -->
+<!-- <script src="/js/login.js">
+</script> -->
 </head>
 <body>
     <p>
         <div class="title"><h4>게시판</h4></div>
     </p>
+    <?php
+                if(!$userid){
+                    ?>
+                    <ul>
+                    <li>
+                    <a href='#' role="button">접속하기<span class="caret"></span></a>
+                    <ul>
+                    <li><a href="login.php">로그인</a></li>
+                    </li>
+                    <li><a href="joinMember.php">회원가입</a></li>
+                    </ul>
+                    </li>
+                    </ul>
+                    <?php
+                }else{
+$logged = $username."(".$userid.")";
+                    ?>
+                    <ul>
+                    <li>
+                    <a href="#" role="button"><b><?=$logged?></b>님의 회원관리
+                    <span class="caret"></span></a>
+<ul>
+<li><a href="logout.php">로그아웃</a></li>
+</ul>
+
+                    </li>
+                    </ul>
+                    <?php
+                }?>
     <form action="writeImageProcess.php" method="POST">
         <p>
             <table class="writeTable" style="text-align: center; border: 1px solid #ddddda">
