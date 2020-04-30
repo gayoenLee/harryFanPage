5f19fb8ff41be50d29f748dfde4ebc4d3d956b80
gayoenLee <leeedho67@gmail.com>
2020-04-30 15:37:23 +0900

    [MOD]글 목록 가져오는 while문 위치 수정

--- imageBoardPage.php
+++ imageBoardPage.php
@@ -12,29 +12,14 @@ else{
     $page = 1;
 }
 ?>
-<?php
-// 저장된 내용 가져오기
-$sqlSecond = database(
-    //테이블로부터 num기준으로 내림차순으로 정렬해서 모든 정보 가져오기
-"SELECT * FROM talkBoard ORDER BY num DESC");
-while(
-    //fetch_aray : mysql 레코드 가져오기. 배열로 가져옴.
-    //https://blog.naver.com/diceworld/220295811114
-    $talkBoard = $sqlSecond->fetch_array()
-){
-    //배열로 저장.
-$title = $talkBoard["title"];
-// 글자수가 30이 넘으면 ...처리
-if(strlen($title)>30){
-    $title = str_replace($board["title"], mb_substr($talkBoard["title"],0,30,"utf-8")."...",$talkBoard["title"]);
-}
-?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <title>이미지 공유 게시판</title>
-        <link rel="stylesheet" href="imageBoardPageCSS.css">
+        <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+        <!-- <link rel="stylesheet" href="imageBoardPageCSS.css"> -->
 <script>
 $(function(){
     //span의 클래스인 readCheck가 클릭 이벤트가 발생하면 자신의 속성 값인 data-action값을 새로운 변수인 actionURL에 저장하고 그 링크로 이동하게 함.
@@ -44,13 +29,9 @@ $(location).attr("href", actionURL);
 
 });
 });
-
 </script>
-
-
     </head>
     <body>
-    <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
         <div id="page" style="height: auto !important;">
         <header id="headerImage" class="site-header" role="banner">   
 <h1 class="siteTitle">
@@ -69,7 +50,8 @@ $(location).attr("href", actionURL);
 		</nav><!-- #site-navigation -->
 			<section class="container">
 <div class="content">
-        <h1>게시판</h1>
+    <div>
+        <h1><b>게시판</b></h1>
         <div class="writeBtn">
         <button  class="write" onclick="location.href='writeImageBoard.php'">글쓰기</button>
         </div>
@@ -84,7 +66,42 @@ $(location).attr("href", actionURL);
                 <th style="background-color: #eeeeee; text-align: center;">조회수</th>
 
             </tr>
+            <?php
+// 저장된 내용 가져오기
+$sqlSecond = database(
+    //테이블로부터 num기준으로 내림차순으로 정렬해서 모든 정보 가져오기
+"SELECT * FROM talkBoard ORDER BY num DESC");
+while(
+    //fetch_aray : mysql 레코드 가져오기. 배열로 가져옴.
+    //https://blog.naver.com/diceworld/220295811114
+    $talkBoard = $sqlSecond->fetch_array()
+){
+    //배열로 저장.
+$title = $talkBoard["title"];
+// 글자수가 30이 넘으면 ...처리
+if(strlen($title)>30){
+    $title = str_replace($board["title"], mb_substr($talkBoard["title"],0,30,"utf-8")."...",$talkBoard["title"]);
+}
+?>
 
+<!-- 글 목록 가져오기 -->
+<tbody>
+    <tr>
+<td width="70"><?=$board['num']; ?></td>
+<td width = "500">
+<!-- data-action은 커스텀 속성., 클릭한 글의 번호에 해당하는 글을 읽는 페이지로 이동하겠다는 것. -->
+    <span class="readCheck" style="cursor:pointer" 
+    data-action="./showImageBoardContents.php?num=<?=$talkBoard['num']?>"><?=$title?></span>
+    <td width="120"><?=$talkBoard['name'];?></td>
+    <td width="100"><?=$talkBoard['time'];?></td>
+    <td width="100"><?=$talkBoard['view'];?></td>
+</tr>
+</tbody>
+<?php
+//while문이 끝날 때까지 게시판테이블 배열의 정보를 가져옴.
+}?>
+</table>
+</div>
             <!-- 페이징 디비로 구현 시작 -->
 <?php
 $sql = database("SELECT * FROM talkBoard");
@@ -118,23 +135,6 @@ $sqlSecond = database(
 ?>
 
 
-<!-- 글 목록 가져오기 -->
-<tbody>
-    <tr>
-<td width="70"><?=$board['num']; ?></td>
-<td width = "500">
-<!-- data-action은 커스텀 속성., 클릭한 글의 번호에 해당하는 글을 읽는 페이지로 이동하겠다는 것. -->
-    <span class="readCheck" style="cursor:pointer" 
-    data-action="./showImageBoardContents.php?num=<?=$talkBoard['num']?>"><?=$title?></span>
-    <td width="120"><?=$talkBoard['name'];?></td>
-    <td width="100"><?=$talkBoard['time'];?></td>
-    <td width="100"><?=$talkBoard['view'];?></td>
-</tr>
-</tbody>
-<?php
-//while문이 끝날 때까지 게시판테이블 배열의 정보를 가져옴.
-}?>
-</table>
 
 <div id="pageNum" style="text-align:center;">
 <?php
@@ -143,7 +143,7 @@ if($page <=1){
 }else{
     echo "<a href='imageBoardPage.php?page=1'>처음</a>";
 }
-if(page<=1){
+if($page<=1){
     //빈 값
 }else{
     $present = $page - 1;
@@ -171,7 +171,7 @@ if($page >= $totalPage){
 </div>
     </div>
     <aside>
-       
+       사이드
     </aside>
     </section>
         </body>
