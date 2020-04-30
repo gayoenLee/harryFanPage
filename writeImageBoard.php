24febe7fc6cb430e7a17cbc134d13de2487199a9
gayoenLee <leeedho67@gmail.com>
2020-04-30 15:39:48 +0900

    [ADD]jquery사용코드 추가

--- writeImageBoard.php
+++ writeImageBoard.php
@@ -7,6 +7,7 @@ include "loginOK.php";
 <head>
     <title>이미지 공유 게시판</title>
     <link rel="stylesheet" type="text/css" href="showImageBoardCSS.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
 </head>
 <body>
     <p>
