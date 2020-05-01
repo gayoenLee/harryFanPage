01452b3ecc99cf773688f3c16ccc51d98eaab0b0
gayoenLee <leeedho67@gmail.com>
2020-05-02 00:10:02 +0900

    [MOD]댓rm.pjpgitcom
    ;

--- commentaAddForm.php
+++ commentaAddForm.php
@@ -1,32 +1,45 @@
-<?php
-// $test = $_GET["title"];
-// $comment = $_POST["comment"];
-// $email = $_POST["email"];
-// $title = $_POST["time"]+$_POST["title"];
-// $time = $_POST["time"];
-// 데이터 받기
-//$data = file_get_contents("imageBoardCommentData/".$date);
-//$array = unserialize($data);
+<form action="commentUpdate.php" method="post">
 
+	<input type="hidden" name="contentNum" value="<?php echo $contentNum?>">
 
-?>
-<!-- 댓글 추가되는 곳 -->
-<!doctype html>
-<body>
-<div>
-<p>
+	<table>
 
-   <div>
-    <?php
-                       // echo $_POST["email"];
-                   
-                    echo printCommentList();
-                           // echo $date;
-                            ?>
-                            </div>
-</p>
-</div>
+		<tbody>
 
+			<tr>
 
-</body>
-</html>
+				<th scope="row"><label for="commentId">아이디</label></th>
+
+				<td><input type="text" name="commentId" id="commentId"></td>
+
+			</tr>
+
+			<tr>
+
+				<th scope="row">
+
+			<label for="commentPassword">비밀번호</label></th>
+
+				<td><input type="password" name="commentPassword" id="commentPassword"></td>
+
+			</tr>
+
+			<tr>
+
+				<th scope="row"><label for="commentContent">내용</label></th>
+
+				<td><textarea name="commentContent" id="commentContent"></textarea></td>
+
+			</tr>
+
+		</tbody>
+
+	</table>
+
+	<div class="btnSet">
+
+		<input type="submit" value="댓글 작성">
+
+	</div>
+
+</form>
