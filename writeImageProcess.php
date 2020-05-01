0f30e56f96b3470a89d738384058c63c94472ef0
gayoenLee <leeedho67@gmail.com>
2020-05-01 13:05:03 +0900

    [MOD]디비게시판 글 추가  insert문 오류 해결

--- writeImageProcess.php
+++ writeImageProcess.php
@@ -1,38 +1,38 @@
 <?php
-// ob_start();
-// $userName=$_POST['userName'];
-// $contents=$_POST['contents'];
-// $title=$_POST['title'];
-// $password=$_POST['password'];
-// $boardTime=$_POST['boardTime'];
-
-// $boardArray = array($title, $userName, $contents, $password, $boardTime);
-// $data = serialize($boardArray);
-
-// file_put_contents('imageBoardData/'.$_POST["title"], $data);
-// header('Location: /imageBoardPage.php?title='.$_POST["title"]);
 session_start();
-include "config.php";
-include "dbConnect.php";
+include 'config.php';
+include 'dbConnect.php';
+
 //세션에 저장된 아이디값을 name에 저장.
-$name = $userid;
-$date = date('Y-m-d');
-$userPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$uid = $userid;
+$utime = date('Y-m-d');
+$upassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
 //앞에서 포스트로 받은 값들 저장.
-$title = $_POST['title'];
-$content = $_POST['content'];
-
+$utitle = $_POST['title'];
+$ucontent = $_POST['content'];
+//삭제 시 번호가 비워지지 않게 하기 위해 작성.
+database("alter table talkBoard auto_increment = 1");
 database(
-    "INSERT talkBoard SET 
-    name = '".$name."';
-    password = '".$userPassword."';
-    title = '".$title."';
-    content = '".$content."';
-    time = '".$date."';
-");
-
-?>
-<script>
-    alert("글쓰기가 완료되었습니다.");
+    "INSERT INTO talkBoard
+    (id, password, title, content, time, view) VALUES ('$uid',
+    '$upassword',
+    '$utitle',
+    '$ucontent',
+    '$utime', 0)
+    ");
+   
+    echo"
+    <script>
+    alert('글쓰기가 완료되었습니다.');
     location.href = 'imageBoardPage.php';
-</script>
+</script>  
+";
+?>
+ 
+<!doctype html>
+
+<body>
+
+</body>
+
+</html>
