838fea1caf99b7e2080b88f73f3cf2507d008301
gayoenLee <leeedho67@gmail.com>
2020-05-01 15:34:32 +0900

    [MOD]게시글 삭제디비 연동해서 구현, 이전 코드 삭제

--- deleteImageBoard.php
+++ deleteImageBoard.php
@@ -1,4 +1,15 @@
 <?php
-unlink('imageBoardData/'.$_POST["time"].$_POST["title"]);
-header('Location: /imageBoardPage.php');
+include "dbConnect.php";
+$contentNum = $_GET['num'];
+//받아온 num값을 이용해서 게시글 삭제
+database(
+    "
+    DELETE FROM talkBoard
+    WHERE num=$contentNum
+    "
+);
 ?>
+<script>
+alert('선택한 게시글이 삭제되었습니다.');
+</script>
+<meta http-equiv="refresh" content="0 url=imageBoardPage.php">
