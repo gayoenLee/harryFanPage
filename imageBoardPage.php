a4ca21e55ab4859c00840aa3b2bc1d7edb5cf5ec
gayoenLee <leeedho67@gmail.com>
2020-05-03 23:01:17 +0900

    [MOD]페이징 오류 수정 완료

--- imageBoardPage.php
+++ imageBoardPage.php
@@ -15,7 +15,6 @@ else{
     $page = 1;
 }
 
-
 ?>
 
 
@@ -155,43 +154,8 @@ break;
                 <th style="background-color: #eeeeee; text-align: center;">조회수</th>
 
             </tr>
-            <?php
-// 저장된 내용 가져오기
-$sqlSecond = database(
-    //테이블로부터 num기준으로 내림차순으로 정렬해서 모든 정보 가져오기
-"SELECT * FROM talkBoard ORDER BY num DESC");
-while(
-    //fetch_aray : mysql 레코드 가져오기. 배열로 가져옴.
-    //https://blog.naver.com/diceworld/220295811114
-    $talkBoard = $sqlSecond->fetch_array()){
-    //배열로 저장.
-$title = $talkBoard["title"];
-// 글자수가 30이 넘으면 ...처리
-// if(strlen($title)>30){
-//     $title = str_replace($talkBoard["title"], mb_substr($talkBoard["title"],0,30,"utf-8")."...",$talkBoard["title"]);
-// }
-?>
-
-<!-- 글 목록 가져오기 -->
-<tbody>
-    <tr>
-<td width="70"><?=$talkBoard['num']; ?></td>
-<td width = "500">
-<!-- data-action은 커스텀 속성., 클릭한 글의 번호에 해당하는 글을 읽는 페이지로 이동하겠다는 것. -->
-    <span class="readCheck" style="cursor:pointer" 
-    data-action="./showImageBoardContents.php?num=<?=$talkBoard['num']?>"><?=$title?></span>
-    <td width="120"><?=$talkBoard['id'];?></td>
-    <td width="100"><?=$talkBoard['time'];?></td>
-    <td width="100"><?=$talkBoard['view'];?></td>
-</tr>
-</tbody>
-<?php
-//while문이 끝날 때까지 게시판테이블 배열의 정보를 가져옴.
-}?>
-</table>
-</div>
-            <!-- 페이징 디비로 구현 시작 -->
-<?php
+               <!-- 페이징 디비로 구현 시작 -->
+               <?php
 $sql = database("SELECT * FROM talkBoard");
 //mysqli_num_rows : 게시판 테이블에 있는 모든 레코드 수를 변수에 저장.
 $totalRecord = mysqli_num_rows($sql);
@@ -220,9 +184,43 @@ $sqlSecond = database(
     "SELECT * FROM talkBoard ORDER BY num DESC LIMIT $pageStart, $list"
 );
 ?>
+            <?php
+// 저장된 내용 가져오기
+
+while(
+    //fetch_aray : mysql 레코드 가져오기. 배열로 가져옴.
+    //https://blog.naver.com/diceworld/220295811114
+    $talkBoard = $sqlSecond->fetch_array()){
+    //배열로 저장.
+$title = $talkBoard["title"];
+// 글자수가 30이 넘으면 ...처리
+// if(strlen($title)>30){
+//     $title = str_replace($talkBoard["title"], mb_substr($talkBoard["title"],0,30,"utf-8")."...",$talkBoard["title"]);
+// }
+?>
+         
+<!-- 글 목록 가져오기 -->
+<tbody>
+    <tr>
+<td width="70"><?=$talkBoard['num']; ?></td>
+<td width = "500">
+<!-- data-action은 커스텀 속성., 클릭한 글의 번호에 해당하는 글을 읽는 페이지로 이동하겠다는 것. -->
+    <span class="readCheck" style="cursor:pointer" 
+    data-action="./showImageBoardContents.php?num=<?=$talkBoard['num']?>"><?=$title?></span>
+    <td width="120"><?=$talkBoard['id'];?></td>
+    <td width="100"><?=$talkBoard['time'];?></td>
+    <td width="100"><?=$talkBoard['view'];?></td>
+</tr>
+</tbody>
+<?php
+//while문이 끝날 때까지 게시판테이블 배열의 정보를 가져옴.
+}?>
+</table>
+</div>
 
 <div id="pageNum" style="text-align:center;">
 <?php
+
 if($page <=1){
     //빈 값
 }else{
@@ -232,6 +230,7 @@ if($page<=1){
     //빈 값
 }else{
     $present = $page - 1;
+
     echo"<a href='imageBoardPage.php?page=$present'>이전</a>";
 }
 for($i = $blockStart; $i <= $blockEnd; $i++){
@@ -252,6 +251,7 @@ if($page >= $totalPage){
 }else{
     echo "<a href='imageBoardPage.php?page=$totalPage'>마지막</a>";
 }
+
 ?>
 </div>
     </div>
