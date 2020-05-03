7bb7975cdc04c5a4e4747ae60a41e1095dd9672e
gayoenLee <leeedho67@gmail.com>
2020-05-03 17:51:32 +0900

    [MOD]디비 연동해서 댓글 수정하는 기능 다시 구현

--- editImageBoardCommentProcess.php
+++ editImageBoardCommentProcess.php
@@ -1,13 +1,15 @@
 <?php
-$comment = $_POST["comment"];
-$email = $_POST["email"];
-$title = $_POST["title"];
-$time = $_POST["time"];
+include 'config.php';
+include 'dbConnect.php';
 
-$commentArray = array($title, $email, $comment, $time);
-$data = serialize($commentArray);
-//rename('imageBoardCommentData/'.$_POST['oldTitle'], 'imageBoardCommentData/'.$_POST['title']);
-unlink('imageBoardCommentData/'.$_POST['editTitle']);
-file_put_contents('imageBoardCommentData/'.$_POST['editTitle'], $_POST['contents']);
-header('Location: /showImageBoardContents.php?title='.$_POST['url']);
-?>
+$rno = $_POST['rno']; 
+$sql = database("select * from commentTable where num='".$rno."'");
+$reply = $sql->fetch_array();
+
+$bno = $_POST['b_no'];
+$sql2 = database("select * from talkBoard where num='".$bno."'");
+$board = $sql2->fetch_array();
+
+$sql3 = database("update commentTable set comment='".$_POST['content']."' where num = '".$rno."'"); ?>
+<script type="text/javascript">alert('수정되었습니다.'); location.replace("showImageBoardContents.php?num=<?php echo $bno; ?>");</script>
+
