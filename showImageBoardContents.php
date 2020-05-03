a97b647effaaec5034749e32d56e54ecaa8c36cc
gayoenLee <leeedho67@gmail.com>
2020-05-03 23:11:56 +0900

    [MOD]이미지 리사이즈하는 함수 추가

--- showImageBoardContents.php
+++ showImageBoardContents.php
@@ -70,7 +70,23 @@ if(isset($_COOKIE['todayViewBoardCookie'.$userid])){
         setcookie('todayViewBoardCookie'.$userid, $todayViewBoard.",".$cookieContentNum, time()+21600, "/");
     }
 }
-
+function resize_image($file, $newfile, $w, $h) {
+    list($width, $height) = getimagesize($file);
+    if(strpos(strtolower($file), ".jpg"))
+       $src = imagecreatefromjpeg($file);
+    else if(strpos(strtolower($file), ".png"))
+       $src = imagecreatefrompng($file);
+    else if(strpos(strtolower($file), ".gif"))
+       $src = imagecreatefromgif($file);
+    $dst = imagecreatetruecolor($w, $h);
+    imagecopyresampled($dst, $src, 0, 0, 0, 0, $w, $h, $width, $height);
+    if(strpos(strtolower($newfile), ".jpg"))
+       imagejpeg($dst, $newfile);
+    else if(strpos(strtolower($newfile), ".png"))
+       imagepng($dst, $newfile);
+    else if(strpos(strtolower($newfile), ".gif"))
+       imagegif($dst, $newfile);
+ }
 ?>
 
 
