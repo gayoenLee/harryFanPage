02e3f8ae4c3b8769bfcfec10f0ad471839438648
gayoenLee <leeedho67@gmail.com>
2020-05-04 21:32:29 +0900

    [MOD]config.php include 삭제

--- editImageBoardCommentProcess.php
+++ editImageBoardCommentProcess.php
@@ -1,15 +1,14 @@
 <?php
-include 'config.php';
 include 'dbConnect.php';
 
 $rno = $_POST['rno']; 
-$sql = database("select * from commentTable where num='".$rno."'");
+$sql = database("select * from commentTable where num=$rno");
 $reply = $sql->fetch_array();
 
 $bno = $_POST['b_no'];
-$sql2 = database("select * from talkBoard where num='".$bno."'");
+$sql2 = database("select * from talkBoard where num=$bno");
 $board = $sql2->fetch_array();
 
-$sql3 = database("update commentTable set comment='".$_POST['content']."' where num = '".$rno."'"); ?>
+$sql3 = database("update commentTable set comment='".$_POST['content']."' where num = $rno"); ?>
 <script type="text/javascript">alert('수정되었습니다.'); location.replace("showImageBoardContents.php?num=<?php echo $bno; ?>");</script>
 
