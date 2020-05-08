3a1689750fc99e4a263c87c786488112755f7276
gayoenLee <leeedho67@gmail.com>
2020-05-08 16:30:42 +0900

    [ADD]동영상 업로드 버튼 클릭후 데이터베이스에 저장하는 코드 추가

---
+++ uploadVideoProcess.php
@@ -0,0 +1,22 @@
+<?php
+include 'dbConnect.php';
+//동영상 저장하는 페이지에서 받은 포스트값들 저장
+$title = $_POST['title'];
+$location = $_POST['location'];
+$duration = $_POST['duration'];
+$time = date('Y-m-d');
+
+//동영상 삭제시 번호가 비워지지 않게 하기 위해 작성
+database("alter table videos auto_increment = 1");
+database(
+    "INSERT INTO videos
+    (title, location, duration, time) VALUES
+    ('$title', '$location', '$duration', '$title')
+");
+echo "<script>
+location.href='uploadVideo.php?load=<?php ?>';
+</script>";
+
+
+?>
+<script type="text/javascript">location.replace("uploadVideo.php?load=<?php echo $title; ?>");</script>
