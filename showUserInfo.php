6fec3b005519898899292bcfe4911bd80a21fa2f
gayoenLee <leeedho67@gmail.com>
2020-05-08 14:54:25 +0900

    [MOD]os,데이터 통계 보여주는 코드 완성

--- showUserInfo.php
+++ showUserInfo.php
@@ -25,21 +25,79 @@ $sqlSafari = database(
 );
 $safariResult = mysqli_fetch_array($sqlSafari);
 $safari = $safariResult[0];
-////////os
+////////os 카테고리
 $osTotal = database(
   "SELECT COUNT(os) from statistics"
 );
+$osTotal = mysqli_fetch_array($osTotal);
+$os = $osTotal[0];
+
 $sqlLinux = database(
   "SELECT COUNT(os) from statistics WHERE os='linux'"
 );
+
+$linuxResult = mysqli_fetch_array($sqlLinux);
+$linux = $linuxResult[0];
 $sqlMac = database(
   "SELECT COUNT(os) from statistics WHERE os='mac'"
 );
+$macResult = mysqli_fetch_array($sqlMac);
+$mac = $macResult[0];
 $sqlWindow = database(
-  "SELECT COUNT(os) from statistics WHERE os='win32'"
+  "SELECT COUNT(os) from statistics WHERE os='windows'"
 );
+$windowResult = mysqli_fetch_array($sqlWindow);
+$window = $windowResult[0];
+
 $browserOthers = $browser - $mozila - $chrome - $linux;
-$osOthers = $osTotal - $sqlLinux - $sqlMac - $sqlWindow;
+$osOthers = $os - $linux - $mac - $window;
+// 요일별로 언제 많이 들어왔는지 알아보기 위해 데이터베이스 이용하기
+$sqlMonday = database(
+  "SELECT COUNT(dayOfWeek) from statistics WHERE dayOfWeek='월요일'"
+);
+$mondayResult = mysqli_fetch_array($sqlMonday);
+$monday = $mondayResult[0];
+//화
+$sqlTuesday = database(
+  "SELECT COUNT(dayOfWeek) from statistics WHERE dayOfWeek='화요일'"
+);
+$tuesdayResult = mysqli_fetch_array($sqlTuesday);
+$tuesday = $tuesdayResult[0];
+//수요일
+$sqlWednesday = database(
+  "SELECT COUNT(dayOfWeek) from statistics WHERE dayOfWeek='수요일'"
+);
+$wednesdayResult = mysqli_fetch_array($sqlWednesday);
+$wednesday = $wednesdayResult[0];
+//목요일
+$sqlThursday = database(
+  "SELECT COUNT(dayOfWeek) from statistics WHERE dayOfWeek='목요일'"
+);
+$thursdayResult = mysqli_fetch_array($sqlThursday);
+$thursday = $thursdayResult[0];
+//금요일
+$sqlFriday = database(
+  "SELECT COUNT(dayOfWeek) from statistics WHERE dayOfWeek='금요일'"
+);
+$fridayResult = mysqli_fetch_array($sqlFriday);
+$friday = $fridayResult[0];
+//토요일
+$sqlSaturday = database(
+  "SELECT COUNT(dayOfWeek) from statistics WHERE dayOfWeek='토요일'"
+);
+$saturdayResult = mysqli_fetch_array($sqlSaturday);
+$saturday = $saturdayResult[0];
+//일요일
+$sqlSunday = database(
+  "SELECT COUNT(dayOfWeek) from statistics WHERE dayOfWeek='일요일'"
+);
+$sundayResult = mysqli_fetch_array($sqlSunday);
+$sunday = $sundayResult[0];
+
+
+
+
+
 
 ?>
 
@@ -79,12 +137,11 @@ $osOthers = $osTotal - $sqlLinux - $sqlMac - $sqlWindow;
       function drawChartOS() {
 
 var dataOS = google.visualization.arrayToDataTable([
-  ['Task', 'Hours per Day'],
-  ['Work',     11],
-  ['Eat',      2],
-  ['Commute',  2],
-  ['Watch TV', 2],
-  ['Sleep',    7]
+  ['OS', 'Hours per Day'],
+  ['Linux', <?echo($linux)?>],
+  ['Mac', <?echo($mac)?>],
+  ['Window', <?echo($window)?>],
+  ['Others', <?echo($osOthers)?>],
 ]);
 
 var optionsOS = {
@@ -99,16 +156,18 @@ chartOS.draw(dataOS, optionsOS);
 function drawChartBrowser() {
 
 var dataBrowser = google.visualization.arrayToDataTable([
-  ['Task', 'Hours per Day'],
-  ['Work',     11],
-  ['Eat',      2],
-  ['Commute',  2],
-  ['Watch TV', 2],
-  ['Sleep',    7]
+  ['DAYS', 'Hours per Day'],
+  ['월',  <?echo($monday)?>],
+  ['화',    <?echo($tuesday)?>],
+  ['수',  <?echo($wednesday)?>],
+  ['목', <?echo($thursday)?>],
+  ['금',  <?echo($friday)?>]
+  ['토', <?echo($saturday)?>],
+  ['일', <?echo($sunday)?>]
 ]);
 
 var optionsBrowser = {
-  title: 'Browser'
+  title: 'Days'
 };
 
 var chartBrowser = new google.visualization.PieChart(document.getElementById('piechartBrowser'));
