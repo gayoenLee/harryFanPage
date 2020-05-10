ad857da968e91a0801acf14b02cd3e3b6626a3df
gayoenLee <leeedho67@gmail.com>
2020-05-10 16:17:47 +0900

    [MOD]jpg이미지 업로드 오류 해결

--- showImageBoardContents.php
+++ showImageBoardContents.php
@@ -75,6 +75,8 @@ function resize_image($file, $newfile, $w, $h) {
     list($width, $height) = getimagesize($file);
     if(strpos(strtolower($file), ".jpg"))
        $src = imagecreatefromjpeg($file);
+       else if(strpos(strtolower($file), ".jpeg"))
+       $src = imagecreatefromjpeg($file);
     else if(strpos(strtolower($file), ".png"))
        $src = imagecreatefrompng($file);
     else if(strpos(strtolower($file), ".gif"))
@@ -82,6 +84,8 @@ function resize_image($file, $newfile, $w, $h) {
     $dst = imagecreatetruecolor($w, $h);
     imagecopyresampled($dst, $src, 0, 0, 0, 0, $w, $h, $width, $height);
     if(strpos(strtolower($newfile), ".jpg"))
+       imagejpeg($dst, $newfile);
+       else if(strpos(strtolower($newfile), ".jpeg"))
        imagejpeg($dst, $newfile);
     else if(strpos(strtolower($newfile), ".png"))
        imagepng($dst, $newfile);
